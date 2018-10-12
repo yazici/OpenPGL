fcc1b23d2867926aa9c25d7a988bd9f5adae8c9b
Maxim <meetmorrowsolonmars@gmail.com>
2018-10-12 22:33:43 +0700

    Create test branch.

--- source/renderer/VAO.cpp
+++ source/renderer/VAO.cpp
@@ -1,41 +1,56 @@
-#include <GL/glew.h>
-
 #include "VAO.h"
-#include "../data/Mesh.h"
 
 namespace pgl
 {
-	VertexObject::VertexObject(const Mesh & mesh)
+	VertexObject::VertexObject(const Mesh &mesh, const vaoOption& parametrs )
 	{
-		/*glGenVertexArrays(1, &_vao);
+		glGenVertexArrays(1, &_vao);
 		glBindVertexArray(_vao);
+
 		_vbos.resize(3);
-		
-		_vbos[0].create(mesh.vertices().size() * sizeof(vec3), mesh.vertices().data());
-		_vbos[0].bind();
-		glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, nullptr);
-		
-		_vbos[1].create(mesh.normals().size() * sizeof(vec3), mesh.normals().data());
-		_vbos[1].bind();
-		glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 0, nullptr);
 
-		_vbos[2].create(mesh.uvs().size() * sizeof(vec2), mesh.uvs().data());
-		_vbos[2].bind();
-		glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 0, nullptr);
-		_vbos[2].unbind();
+		if (parametrs.flagVertices)
+		{
+			/**
+			* TODO: VBO работает только типом float.
+			*/
+			_vbos[0].create(mesh.vertices().size() * sizeof(vec3), mesh.vertices().data());
+			_vbos[0].bind();
+			glVertexAttribPointer(0, 3, parametrs.dataType, parametrs.normalizeData, parametrs.bufSize,nullptr);
+		}
 
-		_indices.create(mesh.triangles());
-		glBindVertexArray(0);*/
+		if (parametrs.flagNormals)
+		{
+			/**
+			* TODO: VBO работает только типом float.
+			*/
+			_vbos[1].create(mesh.normals().size() * sizeof(vec3), mesh.normals().data());
+			_vbos[1].bind();
+			glVertexAttribPointer(1, 3, parametrs.dataType, parametrs.normalizeData, parametrs.bufSize, nullptr);
+		}
+
+
+		if (parametrs.flagUV)
+		{
+			/**
+			* TODO: VBO работает только типом float.
+			*/
+			_vbos[2].create(mesh.uvs().size() * sizeof(vec2), mesh.uvs().data());
+			_vbos[2].bind();
+			glVertexAttribPointer(1, 2, parametrs.dataType, parametrs.normalizeData, parametrs.bufSize, nullptr);
+		}
+		
+		glBindVertexArray(0);
+
 	}
 
 	VertexObject::~VertexObject()
 	{
 		glDeleteVertexArrays(1, &_vao);
 	}
-	
+
 	void VertexObject::draw() const
 	{
 		glBindVertexArray(_vao);
-		glDrawElements(GL_TRIANGLES, _indices.size(), GL_UNSIGNED_INT, nullptr);
 	}
 }
--- source/renderer/VAO.h
+++ source/renderer/VAO.h
@@ -6,45 +6,73 @@
 
 #include "IndexBuffer.h"
 #include "VertexBuffer.h"
+#include "Mesh.h"
 
 namespace pgl
 {
-    using std::vector;
+	using std::vector;
 
-    class Mesh;
+	class Mesh;
 
-    class VertexObject
-    {
-    public:
+	class VertexObject
+	{
+	public:
 
-        /**
-        * Создает OpenGL объект, который описывает данные вершин и используется для рендера этих 
-        * данных.
-        * 
-        * @param mesh загружаемый для рендера меш.
-        */
-        VertexObject(const Mesh &mesh);
+		struct vaoOption;
+
+		/**
+		* Создает OpenGL объект, который описывает данные вершин и используется для рендера этих
+		* данных.
+		*
+		* @param mesh загружаемый для рендера меш.
+		*/
+		VertexObject(const Mesh &mesh, const vaoOption& parametrs);
 
 		VertexObject(const VertexObject &o) = delete;
 
-        /**
-        * Освобождает выделенные ресурсы в том числе и загруженные при помощи функций OpenGL 
-        * данные вершин.
-        */
-        ~VertexObject();
+		/**
+		* Освобождает выделенные ресурсы в том числе и загруженные при помощи функций OpenGL
+		* данные вершин.
+		*/
+		~VertexObject();
 
-        /**
-        * Отображает примитивы из связанных буферов (VBO). Отображение будет проводиться, если 
-        * VAO находится в активном состоянии (т.е. IsEnable() возвращает true).
-        */
-        void draw() const;
-        //void Draw(const Shader &shader);
+		/**
+		* Отображает примитивы из связанных буферов (VBO). Отображение будет проводиться, если
+		* VAO находится в активном состоянии (т.е. IsEnable() возвращает true).
+		*/
+		void draw() const;
+		//void Draw(const Shader &shader);
 
-    private:
-        vector<VertexBuffer> _vbos;
-        IndexBuffer _indices;
-        uint32_t _vao;
-    };
+	private:
+		vector<VertexBuffer> _vbos;
+		IndexBuffer _indices;
+		uint32_t _vao;
+	};
+
+	/**
+	* Структура предназначенная для описания данных 
+	* в буфере.
+	*/
+	struct VertexObject::vaoOption
+	{
+		/**
+		* Три булевых переменные, отвечающие за то,
+		* какие данные будут передаватьсяю.
+		*/
+		bool flagVertices = false;
+		bool flagNormals = false;
+		bool flagUV = false;
+
+		/**
+		* @field bufSize размер буфера
+		* @field dataType тип данных, которые будут помещены в буфер
+		* @field normalizeData булева переменная (GL_FALSE или GL_TRUE),
+		* отвечающая за нормализацию данных в буфере
+		*/
+		size_t bufSize = 0;
+		GLenum dataType = GL_FLOAT;
+		GLenum normalizeData = GL_FALSE;
+	};
 }
 
 #endif // !_RENDERER_VAO_H
